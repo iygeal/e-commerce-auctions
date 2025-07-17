78af9011984f0bf575e8b6cbbc482e8bb391e4f3
Innocent Anozie <iygeal@gmail.com>
2025-07-18 00:29:32 +0100

    Added the toggle to the template

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create", views.create_listing, name="create_listing"),
     path("listing/<int:listing_id>", views.listing, name="listing"),
+    path("watchlist/<int:listing_id>/",
+         views.toggle_watchlist, name="toggle_watchlist"),
 
 ]
